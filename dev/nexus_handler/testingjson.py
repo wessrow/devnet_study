fee32841b25c9a0d4bb73b2a34168046f8502d74
Wessrow <gurra_gla@hotmail.com>
2020-07-13 17:43:24 +0200

    Adding new venv

--- dev/nexus_handler/testingjson.py
+++ dev/nexus_handler/testingjson.py
@@ -1,6 +1,6 @@
 import json
 
-with open('devnet_env_var.json', 'r') as variables:
+with open('../nexus_handler/devnet_env_var.json', 'r') as variables:
     my_dict = json.load(variables)
 
 print(my_dict['Devnet_Nexus']['ios_nx_ip'])
