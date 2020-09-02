7b5ce89ab853180ebc81dac79deb36d357107111
Wessrow <gurra_gla@hotmail.com>
2020-09-02 15:02:27 +0200

    Added CLI support for commands

--- dev/dna_center/command_runner.py
+++ dev/dna_center/command_runner.py
@@ -1,13 +1,13 @@
 import json
 import time
+import sys
 import requests
 from dna_functions import req, wait_for_task, load_task_result
 
-def send_command():
+def send_command(command="show clock"):
 
     payload = {
-                "name": "show ver",
-                "commands": ["ping 8.8.8.8"],
+                "commands": [f"{command}"],
                 "deviceUuids": ["1cfd383a-7265-47fb-96b3-f069191a0ed5"]
             }
 
@@ -15,14 +15,18 @@ def send_command():
 
     return response
 
-def main():
+def main(command="show clock"):
 
-    taskId = send_command()['response']['taskId']
-    print(taskId)
+    taskId = send_command(command)['response']['taskId']
 
-    response = load_task_result(wait_for_task(taskId))    
+    response = load_task_result(wait_for_task(taskId, 5, 10))    
 
     return response    
 
 if __name__ == "__main__":
-    print(main())
+    if len(sys.argv) < 2:
+        print(main())
+
+    else:
+        print(main(sys.argv[1]))
+        
